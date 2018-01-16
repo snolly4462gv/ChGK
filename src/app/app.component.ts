fb0db188e36860a4543e96d3aa916db589afee35
snolly <kassnollygv4462>
2018-01-17 01:26:54 +0300

    12345

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,14 @@ export class AppComponent implements OnInit{
   GameMatrix:Array<number>; 
   Result:string="";
   QuestCount:number = 12;
+  QFlag:boolean = false;
+  QString:string = "";
+  QCurr:number = 0;
   constructor(private service:MainService){}
 
   ngOnInit(){
     this.GetPlayers();
-    this.GameMatrix = this.NewMatrixArray(2,this.QuestCount);
+    this.GameMatrix = this.NewMatrixArray(2,this.QuestCount,0);
   }
 
   CreateGame() {
@@ -30,12 +33,12 @@ export class AppComponent implements OnInit{
     console.log(this.Players);
   }
 
-  NewMatrixArray(rows,columns){
+  NewMatrixArray(rows,columns,val){
     var arr = new Array();
     for(var i=0; i<rows; i++){
       arr[i] = new Array();
       for(var j=0; j<columns; j++){
-        arr[i][j] = 0;//вместо i+j+1 пишем любой наполнитель. В простейшем случае - null
+        arr[i][j] = val;//вместо i+j+1 пишем любой наполнитель. В простейшем случае - null
       }
     }
     return arr;
@@ -64,6 +67,10 @@ export class AppComponent implements OnInit{
       var z = document.createElement("TD");
       var t = document.createTextNode(i+"");
       z.appendChild(t);
+      z.setAttribute("id","cell_num_q_"+i);
+      z.onclick = ()=>{
+        this.QAnswer(i);
+      };
       document.getElementById("myTr").appendChild(z);
     }
 
@@ -95,6 +102,48 @@ export class AppComponent implements OnInit{
       }
       count++;
     }
+}
+
+QAnswer(i:number){
+  if(this.QFlag)
+    document.getElementById("cell_num_q_"+i).style.backgroundColor="white";
+  else 
+  document.getElementById("cell_num_q_"+i).style.backgroundColor="red";
+  this.QFlag = !this.QFlag;
+  this.QString = "";
+  this.QCurr = i;
+}
+
+QAnswerApl(){
+  console.log(this.QCurr," | ",this.QString);
+  let answers:string[] = this.QString.split(" ");
+  console.log(answers);
+  
+  for (let i=0;i<answers.length;i++){
+    if(+answers[i]-1<this.GameMatrix.length){
+      let player = +answers[i];
+    this.GameMatrix[player-1][this.QCurr-1] = 1;
+    document.getElementById("cell_"+player+"_"+this.QCurr).style.backgroundColor="green";
+    }
+  }
+  console.log(this.GameMatrix);
+  this.QAnswer(this.QCurr);
+}
+QAllAdd(){
+  for(let i=0;i<2;i++){
+    let player = i+1;
+    this.GameMatrix[i][this.QCurr-1] = 1;
+    document.getElementById("cell_"+player+"_"+this.QCurr).style.backgroundColor="green";
+  }
+  this.QAnswer(this.QCurr);
+}
+QAllDel(){
+  for(let i=0;i<2;i++){
+    let player = i+1;
+    this.GameMatrix[i][this.QCurr-1] = 0;
+    document.getElementById("cell_"+player+"_"+this.QCurr).style.backgroundColor="white";
+  }
+  this.QAnswer(this.QCurr);
 }
  
 tableClick(player,question) {
